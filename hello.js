00f911f759406662c541ced54d242ce0489b85a4
feldesj <feldesj@users.noreply.github.com>
2020-09-22 15:02:54 -0400

    modify hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
-console.log("Hello World!");
+console.log("Les bateaux flottent...la plupart du temps");
+console.log(
+  "speak white/soyez à l’aise dans vos mots/nous sommes un peuple rancunier/mais ne reprochons à personne/d’avoir le monopole/de la correction de langage"
+);
+console.log("Michèle Lalonde");
